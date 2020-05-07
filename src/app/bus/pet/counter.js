a88d3c0d23fa5a9153fa6e1bd2d54921ca943b8a
antonmazhuto <mazhuto.anton@tui.ru>
2020-05-07 22:21:06 +0300

    first lesson

---
+++ src/app/bus/pet/counter.js
@@ -0,0 +1,19 @@
+// Core
+import React from "react";
+
+// Hooks
+import { useQueryAvailablePets } from "./hooks/useQueryAvailablePets";
+
+export const Counter = () => {
+    const { loading, error, data } = useQueryAvailablePets();
+
+    if (loading) {
+        return <p>Loading...</p>
+    }
+    if (error) {
+        return <p>We have a problem: {error.message}</p>
+    }
+    return (
+        <p>Available Pets: {data.availablePets}</p>
+    )
+};
